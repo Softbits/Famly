633e3c1228c254f6abf4e351d7236e9424108b8a
Christian Popp Thyme <cpt@softbits.dk>
2021-03-05 16:39:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const email = process.argv[2];
+const password = process.argv[3];
+
+console.log(email,password);
+
+const got = require('got');
+const download = require('download');
+const moment = require('moment');
+
+const authUrl = 'https://app.famly.co/api/login/login/authenticate';
+const feedUrl = 'https://api.famly.co/api/feed/feed/feed';
+
+const credentials = {
+    email: email,
+    password: password
+};
+
+let token;
+
+let olderThan;
+
+async function parseFeed() {
+    // Setup query
+    let searchParams = {
+        accessToken: token
+    };
+    if (olderThan) {
+        searchParams.olderThan = olderThan;
+    }
+
+    // Get feed
+    const feed = JSON.parse((await got(feedUrl, {
+        searchParams: searchParams
+    })).body).feedItems;
+
+    if (feed.length) {
+        // Loop feed and find media
+        for (const post of feed) {
+            if (post.videos.length) {
+                for (const video of post.videos) {
+                    await download(video.videoUrl, 'videos');
+                }
+            }
+            if (post.images.length) {
+                for (const image of post.images) {
+                    await download(image.url_big, 'images');
+                }
+            }
+            olderThan = post.createdDate;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function famly() {
+    console.log('Starting')
+
+    // Authenticate and return token
+    token = JSON.parse((await got.post(authUrl, {
+        json: credentials
+    })).body).accessToken;
+    
+    // Find images and videos in feed
+    while (await parseFeed()) {
+        console.log('Status:', moment(olderThan).format('YYYY-MM-DD'));
+    }
+
+    console.log('Done');
+}
+
+famly();
